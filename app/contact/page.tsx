e60acfcf08ecce497373383c3f162c2a71dedf1f
slobodanbajic9 <slobodan.bajic9@gmail.com>
2024-06-28 15:11:58 +0200

    Implement Resend into Form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,4 @@
 import ContactSection from "@/components/Contact";
-import React from "react";
 
 export default function Contact() {
   const text =
--- components/Form.tsx
+++ components/Form.tsx
@@ -1,30 +1,68 @@
 "use client";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { sendEmail } from "@/lib/resend";
+
+interface FormData {
+  fullName: string;
+  email: string;
+  message: string;
+}
 
 const Form = () => {
-  const send = () => {
-    console.log("Form submission");
+  const [formData, setFormData] = useState<FormData>({
+    fullName: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await sendEmail(formData);
+
+    setFormData({
+      fullName: "",
+      email: "",
+      message: "",
+    });
   };
 
   return (
-    <form action={send} className="flex flex-col gap-8 justify-between w-full">
+    <form
+      onSubmit={handleSubmit}
+      className="flex flex-col gap-8 justify-between w-full">
       <input
         type="text"
-        name="Full name"
+        id="fullName"
+        name="fullName"
         placeholder="Full name"
         className="p-4 rounded-lg text-black dark:bg-white"
+        value={formData.fullName}
+        onChange={handleChange}
         required
       />
       <input
         type="email"
-        name="Email"
+        id="email"
+        name="email"
         placeholder="Email"
         className="p-4 rounded-lg text-black dark:bg-white"
+        value={formData.email}
+        onChange={handleChange}
         required
       />
       <textarea
-        name="Message"
+        name="message"
+        id="message"
         placeholder="Your message"
-        id=""
+        value={formData.message}
+        onChange={handleChange}
         className="p-4 rounded-lg text-black dark:bg-white"
         required></textarea>
       <button
