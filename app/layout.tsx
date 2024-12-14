b308443694211b37a419bed2dfb6bc101bd9efdf
slobodanbajic9 <slobodan.bajic9@gmail.com>
2024-12-14 20:34:57 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { Analytics } from "@vercel/analytics/react";
 import Layout from "@/components/Layout";
 import Navbar from "@/components/Navbar";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
             {children}
           </Layout>
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
